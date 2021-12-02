5b4faec98b30ca5e18ac11825b9ac44f7654ba66
Rudra1227 <89596109+Rudra1227@users.noreply.github.com>
2021-12-01 20:53:18 -0600

    screen shot

--- app.js
+++ app.js
@@ -3,37 +3,40 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 var mongoose = require('mongoose');
 var fish = require("./models/fish");
 
 const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
-mongoose.connect(connectionString, {useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect(connectionString, {
+  useNewUrlParser: true, useUnifiedTopology: true
 });
 
 // server start
-async function recreateDB() 
-{
+async function recreateDB() {
   // Delete everything
   await fish.deleteMany();
   let instance1 = new
-  fish({
-    Name: "Goldfish",
-    colour: "Gold",
-    age: 24
-  });
+    fish({
+      Name: "Goldfish",
+      colour: "Gold",
+      age: 24
+    });
   let instance2 = new
-  fish({
-    Name: "Bluefish",
-    colour: "Blue",
-    age: 22
-  });
+    fish({
+      Name: "Bluefish",
+      colour: "Blue",
+      age: 22
+    });
   let instance3 = new
-  fish({
-    Name: "Swordfish",
-    colour: "Silver",
-    age: 09
-  });  
+    fish({
+      Name: "Swordfish",
+      colour: "Silver",
+      age: 09
+    });
   instance1.save(function (err, doc) {
     if (err) return console.error(err);
     console.log("First object saved")
@@ -71,22 +74,58 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }));
+
+
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/fish',fishRouter);
-app.use('/addmods',addmodsRouter);
-app.use('/selector',selectorRouter);
+app.use('/fish', fishRouter);
+app.use('/addmods', addmodsRouter);
+app.use('/selector', selectorRouter);
 app.use('/', resourceRouter);
 
+
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
